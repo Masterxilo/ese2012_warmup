f6a071f19006b6e9b3edc085ae1798eb9879a08b
Aaron Karper <akarper@students.unibe.ch>
2012-09-21 00:57:38 +0200

    Code review

--- app/trade_item/item.rb
+++ app/trade_item/item.rb
@@ -16,8 +16,24 @@ module TradeItem
         def buy?(buyer)
             if buyer.credits < self.price or not self.active or buyer == self.owner
                 false
-                return
+                return # AK this probably does the right thing by accident:
+								# `return` without anything to return gives `nil`, which is
+								# somewhat false as well. You want
+								# `return false` though. It is possible and used for early returning.
+								# Fully idiomatic it would be:
             end
+						return false if buyer.credits < self.price or not self.active or buyer == self.owner
+						# or if you want to focus on the different options:
+						return false if buyer.credits < self.price
+						return false unless self.active
+						return false if buyer == self.owner
+						# ---
+						
+						# Methods with a question mark are "predicates", that are 
+						# *functions* that *return booleans*. This method does return
+						# booleans, but it has side effects (the owner changes, ...),
+						# therefore it is no function. You can do something like
+						# `buy!(item) if buy?(item)` to fix this.
             owner.credits += self.price
             buyer.credits -= self.price
             self.owner.remove_owned(self)
@@ -29,16 +45,22 @@ module TradeItem
         
         def initialize
             self.price = 0
-            self.name = "<unnamed item>"
+            self.name = "<unnamed item>" # AK prefer nil or ""
             self.active = false
         end
         
         def to_s
-            a = "inactive"
-            if self.active
+            a = "inactive" 
+            if self.active # AK this doesn't look quite right.
                 a = "active"
             end
+						# You could either save the state as a symbol, not a boolean, but
+						# that would mess up other things. But you could also use the
+						# ternary operator ?:
+						
             "Item #{self.name} owned by #{self.owner.name} costs #{self.price} credits is #{a} (for trading)"
+
+            "Item #{self.name} owned by #{self.owner.name} costs #{self.price} credits is #{self.active ? "active" : "inactive"} (for trading)"
         end
     end
 end
--- app/trade_item/user.rb
+++ app/trade_item/user.rb
@@ -23,14 +23,20 @@ module TradeItem
                 if item.active
                     a.push(item)
                 end
-            }
-            a
+            } 
+            a # AK as you can imagine, this is a very common task, so this is the faster way:
+						self.owned_items.select {|item| item.active}
+						# or if you are very economic about characters:
+						owned_items.select(&:active)
         end
         
         def initialize
             self.credits = 100
-            self.name = "<nameless user>"
-            self.owned_items = Array.new
+            self.name = "<nameless user>" # AK you probably shouldn't make it a string. maybe leave it `nil`? or if it needs to be a string, use ""?
+            self.owned_items = Array.new # Ruby uses literals, so you should write
+						#self.owned_items = []
+
+						# you can also take parameters in the constructor, if certain values NEED to be provided by the named constructor (name probably is such a case)
         end
                
         def to_s
